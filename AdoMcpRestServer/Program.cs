4e6c13294a9b442556d4946daa07a1964a2d135e
Andrew Herrick <aj@andrewherrick.com>
2025-12-16 15:42:32 -0500

    Add missing using for Microsoft.AspNetCore.Http
    Added the Microsoft.AspNetCore.Http namespace import to Program.cs to ensure access to HTTP-related types. This may be required for new middleware or endpoint logic.

--- AdoMcpRestServer/Program.cs
+++ AdoMcpRestServer/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
